f8032d9c13f74f502e44fecd6562d53190f44b97
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-06-07 11:16:09 +0300

    added a svg icon component

--- src/components/common/SvgIcon/SvgIcon.tsx
+++ src/components/common/SvgIcon/SvgIcon.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import { FC } from 'react';
 
-const SvgIcon = () => {
-  return <div>SvgIcon</div>;
+import Image from 'next/image';
+
+import ethereumSvg from '../../../../public/svg/ethereum.svg';
+import polyGamingSvg from '../../../../public/svg/poly-game.svg';
+import polyClusterSvg from '../../../../public/svg/poly-cluster.svg';
+import wmaticWthSvg from '../../../../public/svg/wmatic-wth.svg';
+
+export type IconType = 'ethereum' | 'polyGaming' | 'polyCluster' | 'wmaticWth';
+
+const SvgIcon: FC<{
+  type: IconType;
+  height?: number;
+  width?: number;
+  onClick?: () => void;
+}> = ({ type, height, width, onClick }) => {
+  const source = {
+    ethereum: ethereumSvg,
+    polyGaming: polyGamingSvg,
+    polyCluster: polyClusterSvg,
+    wmaticWth: wmaticWthSvg,
+  }[type];
+
+  return (
+    <Image
+      loading="lazy"
+      src={source}
+      height={height}
+      width={width}
+      alt={type}
+    />
+  );
 };
 
 export default SvgIcon;
