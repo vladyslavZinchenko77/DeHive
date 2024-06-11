3e48768048452786f83185a6c5d5a74c814dd7de
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-06-11 20:26:08 +0300

    added title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="test task" />
+        <meta name="keywords" content="deHive, blaze, crypto" />
+        <meta name="author" content="Your Name" />
+        <title>DeHive</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap"
           rel="stylesheet"
